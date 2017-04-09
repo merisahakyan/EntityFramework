90e8a6c8b44cd3345ab1a931637fa3e10982b76b
merisahakyan <sahakyan_m@bk.ru>
2017-04-09 13:00:27 +0400

    CodeFirst

--- CodeFirst/CodeFirst/Program.cs
+++ CodeFirst/CodeFirst/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace CodeFirst
 {
+    public class User
+    {
+        public int ID { get; set; }
+        public string username { get; set; }
+    }
+    public class UsersContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            using (var context = new UsersContext())
+            {
+                var user = new User { username = "name" };
+                context.Users.Add(user);
+                context.SaveChanges();
+            }
         }
     }
 }
