d30d100e5735c04905492185b8fdf0f562a1c8f4
merisahakyan <sahakyan_m@bk.ru>
2017-05-13 20:01:31 +0400

    SQL commands

--- ConnectionStrings/ConnectionStrings/Program.cs
+++ ConnectionStrings/ConnectionStrings/Program.cs
@@ -14,13 +14,18 @@ namespace ConnectionStrings
             SqlConnectionStringBuilder constr = new SqlConnectionStringBuilder();
             constr.DataSource = ".\\SQLEXPRESS";
             constr.InitialCatalog = "Users";
+            constr.IntegratedSecurity = true;
+            
             using (var connection = new SqlConnection())
             {
                 connection.ConnectionString = constr.ConnectionString;
                 connection.Open();
-
+                SqlCommand command = new SqlCommand("SELECT * FROM MyUsers",connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                    Console.WriteLine(reader.FieldCount);
             }
-            
+            Console.ReadLine();
         }
     }
 }
