a9704ff1bad6d90a700993caf9840b0892e6e2dc
merisahakyan <sahakyan_m@bk.ru>
2017-05-13 19:50:30 +0400

    SQL Connections

---
+++ ConnectionStrings/ConnectionStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder constr = new SqlConnectionStringBuilder();
+            constr.DataSource = ".\\SQLEXPRESS";
+            constr.InitialCatalog = "Users";
+            using (var connection = new SqlConnection())
+            {
+                connection.ConnectionString = constr.ConnectionString;
+                connection.Open();
+
+            }
+            
+        }
+    }
+}
