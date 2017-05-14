6f6f7517178e9c4e5d0a7e481316d4d235f3dc03
merisahakyan <sahakyan_m@bk.ru>
2017-05-14 12:27:56 +0400

    adding columns and rows into table (DataTable,DataColumn,DataRow)

---
+++ ColumnsRows/ColumnsRows/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColumnsRows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("UserID", typeof(int));
+            table.Columns.Add("FirstName", typeof(string));
+            table.Columns.Add("LastName", typeof(string));
+            for (int i = 0; i < 3; i++)
+            {
+                table.LoadDataRow(new Object[] { i, $"firstname{i}", $"lastname{i}" }, true);
+                table.AcceptChanges();
+            }
+            for(int i=0;i<table.Rows.Count;i++)
+            {
+                for(int j=0;j<table.Columns.Count;j++)
+                {
+                    Console.WriteLine(table.Columns[j].ColumnName+":"+table.Rows[i][j]);
+                }
+                Console.WriteLine("RowState:" + table.Rows[i].RowState);
+                Console.WriteLine();
+            }
+        }
+    }
+}
