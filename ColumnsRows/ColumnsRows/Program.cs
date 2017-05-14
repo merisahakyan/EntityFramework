58264f7b560efb0583120f1b56771451565ad87c
merisahakyan <sahakyan_m@bk.ru>
2017-05-14 12:58:17 +0400

    Deleting Row,,Changing values RowState and DataRowVersion

--- ColumnsRows/ColumnsRows/Program.cs
+++ ColumnsRows/ColumnsRows/Program.cs
@@ -18,15 +18,32 @@ namespace ColumnsRows
             for (int i = 0; i < 3; i++)
             {
                 table.LoadDataRow(new Object[] { i, $"firstname{i}", $"lastname{i}" }, true);
-                table.AcceptChanges();
+
             }
-            for(int i=0;i<table.Rows.Count;i++)
+            table.Rows[0][0] = 5;
+            table.Rows[1].Delete();
+
+            //table.AcceptChanges();
+            for (int i = 0; i < table.Rows.Count; i++)
             {
-                for(int j=0;j<table.Columns.Count;j++)
+                Console.WriteLine("About ID");
+                if (table.Rows[i].RowState.ToString() != "Deleted")
                 {
-                    Console.WriteLine(table.Columns[j].ColumnName+":"+table.Rows[i][j]);
+                    for (int j = 0; j < table.Columns.Count; j++)
+                    {
+                        Console.WriteLine(table.Columns[j].ColumnName + ":" + table.Rows[i][j]);
+                    }
+
+                    Console.WriteLine("RowState:" + table.Rows[i].RowState);
+                    Console.WriteLine("Original value:" + table.Rows[i][0, DataRowVersion.Original]);
+                    Console.WriteLine("Current value:" + table.Rows[i][0, DataRowVersion.Current]);
+                    Console.WriteLine("Defoult value:" + table.Rows[i][0, DataRowVersion.Default]);
                 }
-                Console.WriteLine("RowState:" + table.Rows[i].RowState);
+
+                else
+                {
+                    Console.WriteLine($"RowState: the {i}th is " + table.Rows[i].RowState);
+                }
                 Console.WriteLine();
             }
         }
